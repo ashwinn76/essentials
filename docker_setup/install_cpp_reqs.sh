5bf16f4a51a2594b2ed6e70e37909573e8306bf5
Ashwin Nair <ashwin.nair@motional.com>
2022-06-08 19:27:02 -0400

    Add more reqs to cpp

--- docker_setup/install_cpp_reqs.sh
+++ docker_setup/install_cpp_reqs.sh
@@ -1,7 +1,11 @@
-sudo apt-get update
+sudo apt update
 
 sudo apt install gcc-10 g++-10
 
 sudo apt install clang-format-11
 
 sudo apt install cppcheck
+
+sudo apt install libcurl4-openssl-dev
+
+sudo apt install nlohmann-json3-dev
